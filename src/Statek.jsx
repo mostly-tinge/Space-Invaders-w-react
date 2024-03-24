b0bdbefb53ba67da8266c22f807882034f1c9161
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-03-24 12:55:50 +0100

    Zaaktualizowano statek

--- src/Statek.jsx
+++ src/Statek.jsx
@@ -1,7 +1,8 @@
-const Statek = () => {
-    return (  
-        <div></div>
+const Statek = ({ruchStatku, marginesStatku, hpStatku}) => {
+    return (
+       <div className={ruchStatku}>
+            <div id="statek" className={`w-12 h-20 text-red-500 mt-4 bg-stone-600 ${marginesStatku}`}>hp: {hpStatku}</div>
+        </div>
     );
 }
- 
 export default Statek;
