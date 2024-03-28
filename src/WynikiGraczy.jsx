fda5bf1210c8ded2e1414cd959cb1a5f13ee82da
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-03-28 11:03:06 +0100

    Update WynikiGraczy.jsx

--- src/WynikiGraczy.jsx
+++ src/WynikiGraczy.jsx
@@ -1,27 +1,23 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
-const bylekto = [
-    {wynik: 14, tekst: 'h89883'}, 
-    {wynik: 55, tekst: 'dfdfd'}
-];
+import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 const WynikiGraczy = () => {
-    console.log(bylekto)
-    const zapytanie = useQuery({
-        queryKey: ["post"],
-        queryFn: () => {
-            wait(1000).then(() => 
-                [...bylekto]
-                )},
-    })
+    const feczuj = async () => {
+        const res = await fetch('http://localhost:4300/wyniki');
+        return res.json();
+    };
+    const {dane, error, isError} = useQuery({ queryKey: ['wynikiGraczy'], queryFn: feczuj});
 
-    if(zapytanie.isLoading) return <h1>łąduje...</h1>
-    if(zapytanie.isError) return <sub>nie udało się</sub>
-    //zapytanie.data.map(pytanie => (
-        //    <div key={pytanie.wynik}>{pytanie.tekst}</div>
-       // ))
+    if(isError) return <h1 className="text-center text-white text-3xl">{JSON.stringify(error)}</h1>
 
-    return ( 
-        <></>
+    return (
+        <div className="flex justify-center mt-44 h-80 opacity-80">
+            <div className="bg-cyan-800 w-40 rounded-2xl space-y-10">
+                {dane ? dane.map(pytanie => (
+                    <div className="text-cyan-400 text-center cursor-pointer transition ease-in-out delay-150 hover:text-cyan-100 duration-300 hover:bg-emerald-500 rounded-2xl" key={pytanie.nazwa}>{pytanie.wynik}- {pytanie.nazwa}</div>
+                )):<h1 className="text-center text-white text-3xl">ładuje...</h1>}
+                <Link className='text-cyan-400 text-center cursor-pointer transition ease-in-out delay-150 hover:text-cyan-100 duration-300 hover:bg-emerald-500 text-sm' to={'/'}>Wróć do strony głównej</Link>
+            </div>
+        </div>
     );
 }
- 
 export default WynikiGraczy;
