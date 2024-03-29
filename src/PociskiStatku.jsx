919c9d17d041acb81c30fc078a52862d17de123b
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-03-29 08:51:07 +0100

    Dodano pociski Statku
    I wreszcie naprawiłem re-renderrowanie ich wszystkich.

---
+++ src/PociskiStatku.jsx
@@ -0,0 +1,20 @@
+import { memo } from "react";
+function PociskiStatku({polozeniePocisku}){
+    const stylPociskow = [`left-4`, `left-16`, `left-28`, `left-44`, `left-56`, 
+    `left-64 ml-4`, `ml-4 left-72`, `ml-5 left-80`, `ml-4 left-96`, 'ml-44 left-96', 
+    `left-72 ml-96`, `right-96 mr-96`, `right-96 mr-80`,  `right-96 mr-64`, `right-96 mr-44`, 
+    `right-72 mr-28`, `right-72 mr-5`, `right-56 mr-1`, `right-44 mr-1`, `right-28`, 
+    `right-12 mr-2`, `right-3`];
+
+    const kluczePociskow = [`ebwboa`, `niwa98w`, `90911`, `11037ca8wo`, `ohca2mvjc`, `d89wadh89wi`, `b89wab`, `bawwadwa98`, 
+    `cbae9wdg9wa`, `nc9a892vhcz`, `a89w99`, `nc90aba`, `ncanobcxc`, `ucag8wa`, `nc9ahwkjc`, `bcua8wwdwa22`, `ba89-x8982`, `b8cw9s1!!`,
+    `cna89ghxzv`, `b79atjhx`, `hc9astw`, `busgnbvcn`];//styl={pocisk} polozenie={polozeniePocisku[index]} key={kluczePociskow[index]
+    return (
+        <>
+            {stylPociskow.map((pocisk, index) => 
+                <div className={`h-24 w-3 bg-slate-700 absolute ${pocisk} ${polozeniePocisku[index]}`} key={kluczePociskow[index]}></div>
+            )}
+       </> 
+     );
+}
+export default memo(PociskiStatku);
