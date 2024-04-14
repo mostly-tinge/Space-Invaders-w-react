e10216cee3fa8e558c3edd5c245fc86130d4458f
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-04-14 19:30:42 +0200

    Jednak musiałem dać ufoludy, aby w refa, aby je przekazać

--- src/PoziomTrudnosci.jsx
+++ src/PoziomTrudnosci.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import LogikaGry from "./LogikaGry"
+import { Link } from "react-router-dom";
 
 const PoziomTrudnosci = () => {
     const [czyWybranoTrudnosc, ustawTrudnosc] = useState(false);
-    const ufoludy = [
+    const ufoludy = useRef([
         {
             klucz: 'bueiowvuwi', tablicaZufo: ['pier', 'drug', 'trze', 'piąt']
         },
@@ -12,26 +13,28 @@ const PoziomTrudnosci = () => {
         },
         {
             klucz: 'bueiowvu', tablicaZufo: ['dziew', 'dzies', 'jede', 'siódviaq']
-        }];
+        }]);
     function ustawKosmitow(kosmiciDoWyjebania){
         for(let i = 0; i < kosmiciDoWyjebania; i++){
-            ufoludy[0].tablicaZufo.pop();
-            ufoludy[1].tablicaZufo.pop();
-            ufoludy[2].tablicaZufo.pop();
+            ufoludy.current[0].tablicaZufo.length -= 1;
+            ufoludy.current[1].tablicaZufo.length -= 1;
+            ufoludy.current[2].tablicaZufo.length -= 1;
         }
-        ustawTrudnosc(true);
+        console.log(ufoludy.current[0].tablicaZufo);
+        setTimeout(() => ustawTrudnosc(true), 1000);
     }
-    if(czyWybranoTrudnosc) return <LogikaGry kosmici={ufoludy}/>
+    console.log(ufoludy.current[0].tablicaZufo + ' grugie');
+    if(czyWybranoTrudnosc) return <LogikaGry ufoludy={ufoludy}/>
     return (
         <div className="flex justify-center text-center">
-            <div className="w-64 bg-slate-700 h-64 rounded-3xl mt-44">
+            <div className="w-64 bg-slate-700 h-80 rounded-3xl mt-44">
                 <div className="grid justify-center text-2xl text-white space-y-7">
-                    <div className={`transition duration-300 ease-in-out delay-150 cursor-pointer`}>Wybierz poziom trudności
+                    Wybierz poziom trudności
                     <div className="space-y-8 mt-4">
                         <p className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl" onClick={() => ustawKosmitow(2)}>Łatwy</p> 
                         <p className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl" onClick={() => ustawKosmitow(1)}>Średni</p> 
                         <p className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl" onClick={() => ustawKosmitow(0)}>Trudny</p>
-                    </div>
+                        <p className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl"><Link to={"/"}>Wróć na stronę główną</Link></p>
                     </div>
                 </div>
             </div>
