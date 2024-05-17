8055de57b781861328c0434104d09641a00f79ab
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-05-17 12:45:51 +0200

    Naprawiono nie pojawianie się napisu przy powrocie i zwiększono menu

--- src/EkranStartowy.jsx
+++ src/EkranStartowy.jsx
@@ -1,16 +1,16 @@
 import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 export default function EkranStartowy(){
+    console.log('hhh')
     const [napis, zmienNapis] = useState([]);
     const [czyOProjekcieJestZaladowane, zaladujOProjekcie] = useState(false);
     const [czyOpcjaJestZaladowana, zaladujOpcje] = useState(false);
     const [czySterowanieJestZaladowane, zaladujSterowanie] = useState(false);
     const ktoryNapis = useRef(-1);
     function animacjaNapisu(){
+        const litery = ['S', 'p', 'a', 'c', 'e', ' ', 'i', 'n', 'v', 'i', 'd', 'e', 'r', 's', ' ', 'w', ' ', 'r', 'e', 'a', 'c', 't', 'i', 'e'];
         let czyMaIscOdKonca = false;
         setInterval(() => {
-            console.log('lal');
-            const litery = ['S', 'p', 'a', 'c', 'e', ' ', 'i', 'n', 'v', 'i', 'd', 'e', 'r', 's', ' ', 'w', ' ', 'r', 'e', 'a', 'c', 't', 'i', 'e'];
             let napisBuforowa = [];
             ktoryNapis.current += 1;
             if(ktoryNapis.current === 25){
@@ -28,6 +28,9 @@ export default function EkranStartowy(){
             zmienNapis(napisBuforowa.join(''));
         }, 800)
     }
+    if(ktoryNapis.current === -1){
+        animacjaNapisu();
+    }
     function zaladujOpcjeZamknijSterowanie(){
         zaladujOpcje(true);
         zaladujSterowanie(false);
@@ -41,30 +44,29 @@ export default function EkranStartowy(){
         zaladujOpcje(false);
         zaladujOProjekcie(false);
     }
-    window.addEventListener('load', animacjaNapisu);
     return (
         <>
-        <div className="text-white text-center mt-16 text-3xl">{(napis.length > 0) ? napis : <div className="opacity-0">P</div>}</div>
-        <div className="flex justify-center h-56 space-y-12 mt-44 opacity-80 ">
-            <div className="text-center w-56 space-y-16 text-xl bg-cyan-800 rounded-3xl">
-                <Link to={`/gra`}><div onClick={() => zaladujOpcje(false)} className="inline text-center transition duration-300 ease-in-out delay-100 cursor-pointer  text-cyan-400 hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl" >Start</div></Link><br></br><br></br><br></br>
-                <div onClick={zaladujSterowanieZamknijOpcje} className="inline text-center transition duration-300 ease-in-out cursor-pointer text-cyan-400 hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl">Sterowanie</div><br></br><br></br><br></br>
-                <div onClick={zaladujOpcjeZamknijSterowanie} className="inline text-center transition duration-300 ease-in-out cursor-pointer  text-cyan-400 hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl">Opcje</div>
+        <div className="text-white text-center mt-16 text-3xl">{(napis.length > 0) ? napis : <div className="opacity-0 absolute">P</div>}</div>
+        <div className="flex justify-center h-64 space-y-12 mt-44 opacity-80 ">
+            <div className="text-center w-64 space-y-16 text-xl bg-cyan-800 rounded-3xl">
+                <Link to={`/gra`}><div onClick={() => zaladujOpcje(false)} className="text-center text-2xl transition duration-300 ease-in-out delay-100 cursor-pointer  text-white hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl" >Start</div></Link>
+                <div onClick={zaladujSterowanieZamknijOpcje} className="text-center text-2xl transition duration-300 ease-in-out cursor-pointer text-white hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl">Sterowanie</div>
+                <div onClick={zaladujOpcjeZamknijSterowanie} className="text-center text-2xl transition duration-300 ease-in-out cursor-pointer  text-white hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl">Opcje</div>
             </div>
-            {czySterowanieJestZaladowane && <div className="w-56 h-56 pt-8 space-y-16 text-center text-cyan-200 bg-cyan-800 rounded-3xl">
-            <p>Ruch w lewo &larr; Ruch w prawo &rarr;</p>Strzał- LPM
-            <p onClick={() => zaladujSterowanie(false)} className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl">Zamknij okno</p>
+            {czySterowanieJestZaladowane && <div className="relative bottom-12 text-xl left-5 w-56 h-64 space-y-10 text-center text-white bg-cyan-800 rounded-3xl">
+            <p>Ruch w lewo &larr;</p>
+            <p>Ruch w prawo &rarr;</p>
+            <p>Strzał- LPM</p>
+            <p onClick={() => zaladujSterowanie(false)} className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl">Zamknij okno</p>
             </div>}
-            {czyOpcjaJestZaladowana && <div className="w-56 h-56 space-y-16 text-center text-cyan-200 bg-cyan-800 rounded-3xl">
-                <div onClick={() => zaladujOProjekcie(true)} className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl">O projekcie</div>
-                <div className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl"><Link to={'/wyniki'}>wyniki graczy</Link></div>
-                <div className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl" onClick={() => zaladujOpcje(false)}>Zamknij okno</div>
+            {czyOpcjaJestZaladowana && <div className="relative bottom-12 text-2xl left-5 w-56 h-64 space-y-16 text-center text-white bg-cyan-800 rounded-3xl">
+                <div onClick={() => zaladujOProjekcie(true)} className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl">O projekcie</div>
+                <div className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl"><Link to={'/wyniki'}>wyniki graczy</Link></div>
+                <div className="transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl" onClick={() => {zaladujOpcje(false); zaladujOProjekcie(false)}}>Zamknij okno</div>
             </div>}
-            {czyOProjekcieJestZaladowane && <div className="text-center text-cyan-200 bg-cyan-800 w-80 rounded-3xl h-72 space-y-9">Jest to mój pierwszy projekt w react'ie.<br></br><br></br>
-                Chciałem zrobić coś unikatowego a nie kolejną apke od to-do czy pogody.<br></br>
-                Robienie gier to tylko "symulowane problemy", które sobie dałem, a<br></br><br></br>
-                teraz chcę rozwiązać wiekszy problem- Jak propagować filozofię ze stylem
-                <div className='transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-100 hover:bg-emerald-500 rounded-2xl' onClick={zamknijOba}>Zamknij okno</div>
+            {czyOProjekcieJestZaladowane && <div className="relative bottom-12 left-10 text-center text-xl text-white bg-cyan-800 w-64 rounded-3xl h-72 space-y-9">Jest to mój pierwszy projekt w react'ie.<br></br><br></br>
+                Podstawowym założeniem było zrobić coś unikatowego a nie kolejną apke od to-do czy pogody.<br></br>
+                <div className='transition duration-300 ease-in-out delay-150 cursor-pointer hover:text-cyan-800 hover:bg-emerald-500 rounded-2xl' onClick={zamknijOba}>Zamknij okno</div>
             </div>}
         </div>
         </>
