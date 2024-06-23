91c909533bb047392588c1365a2f7af266aa83c5
mostly-tinge <127690556+mostly-tinge@users.noreply.github.com>
2024-06-23 20:24:56 +0200

    Zmieniono czcionkę

--- src/WynikiGraczy.jsx
+++ src/WynikiGraczy.jsx
@@ -10,12 +10,12 @@ const WynikiGraczy = () => {
     if(isError) return <h1 className="text-center text-white text-3xl">{JSON.stringify(error)}</h1>
 
     return (
-        <div className="flex justify-center mt-44 h-80 opacity-80">
-            <div className="bg-cyan-800 w-40 rounded-2xl space-y-10">
+        <div className={`flex justify-center opacity-80 ${dane ? "h-80 mt-44" : "h-24 mt-64"}`}>
+            <div className="bg-cyan-800 w-48 rounded-2xl space-y-10 font-mono">
                 {dane ? dane.map(pytanie => (
                     <div className="text-cyan-400 text-center cursor-pointer transition ease-in-out delay-150 hover:text-cyan-100 duration-300 hover:bg-emerald-500 rounded-2xl" key={pytanie.nazwa}>{pytanie.wynik}- {pytanie.nazwa}</div>
                 )):<h1 className="text-center text-white text-3xl">ładuje...</h1>}
-                <Link className='text-cyan-400 text-center cursor-pointer transition ease-in-out delay-150 hover:text-cyan-100 duration-300 hover:bg-emerald-500 text-sm' to={'/'}>Wróć do strony głównej</Link>
+                <Link className={'text-cyan-400 text-center cursor-pointer transition ease-in-out delay-150 hover:text-cyan-100 duration-300 hover:bg-emerald-500 rounded-2xl text-xl'} to={'/'}>Wróć do strony głównej</Link>
             </div>
         </div>
     );
